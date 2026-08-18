3dcc51aad8a2595a1d05fa02f959398e5ec7c3aa
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2026-08-18 16:57:27 +0300

    Change layout of beam_profiler_widget.

--- laser_beam_measurements/widgets/image_processing/ui_beam_profiler_widget.py
+++ laser_beam_measurements/widgets/image_processing/ui_beam_profiler_widget.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'beam_profiler_widget_2.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.7.0
+## Created by: Qt User Interface Compiler version 6.8.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,12 +15,12 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QGridLayout,
-    QGroupBox, QHBoxLayout, QHeaderView, QPushButton,
-    QSizePolicy, QSpacerItem, QTableWidgetItem, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDoubleSpinBox,
+    QGridLayout, QGroupBox, QHBoxLayout, QHeaderView,
+    QPlainTextEdit, QPushButton, QSizePolicy, QSpacerItem,
+    QSplitter, QTableWidgetItem, QVBoxLayout, QWidget)
 
-from laser_beam_measurements.widgets.utils.custom_graphics_view import CustomGraphicsView
+from ..utils.custom_graphics_view import CustomGraphicsView
 from laser_beam_measurements.widgets.utils.parameters_table_widget import ParametersTableWidget
 from pyqtgraph import PlotWidget
 
@@ -28,18 +28,79 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(967, 787)
-        self.horizontalLayout_2 = QHBoxLayout(Form)
-        self.horizontalLayout_2.setSpacing(1)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalLayout_2.setContentsMargins(2, 2, 2, 2)
-        self.groupBox = QGroupBox(Form)
-        self.groupBox.setObjectName(u"groupBox")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
+        Form.resize(983, 739)
+        self.horizontalLayout = QHBoxLayout(Form)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.splitter_4 = QSplitter(Form)
+        self.splitter_4.setObjectName(u"splitter_4")
+        self.splitter_4.setOrientation(Qt.Orientation.Horizontal)
+        self.widget = QWidget(self.splitter_4)
+        self.widget.setObjectName(u"widget")
+        self.verticalLayout_4 = QVBoxLayout(self.widget)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.groupBoxCursors = QGroupBox(self.widget)
+        self.groupBoxCursors.setObjectName(u"groupBoxCursors")
+        self.verticalLayout_7 = QVBoxLayout(self.groupBoxCursors)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.groupBoxMode = QGroupBox(self.groupBoxCursors)
+        self.groupBoxMode.setObjectName(u"groupBoxMode")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
-        self.groupBox.setSizePolicy(sizePolicy)
+        sizePolicy.setHeightForWidth(self.groupBoxMode.sizePolicy().hasHeightForWidth())
+        self.groupBoxMode.setSizePolicy(sizePolicy)
+        self.groupBoxMode.setMinimumSize(QSize(120, 48))
+        self.comboBoxMode = QComboBox(self.groupBoxMode)
+        self.comboBoxMode.setObjectName(u"comboBoxMode")
+        self.comboBoxMode.setGeometry(QRect(40, 20, 62, 22))
+
+        self.verticalLayout_7.addWidget(self.groupBoxMode)
+
+        self.groupBoxTop = QGroupBox(self.groupBoxCursors)
+        self.groupBoxTop.setObjectName(u"groupBoxTop")
+        sizePolicy.setHeightForWidth(self.groupBoxTop.sizePolicy().hasHeightForWidth())
+        self.groupBoxTop.setSizePolicy(sizePolicy)
+        self.groupBoxTop.setMinimumSize(QSize(120, 48))
+        self.doubleSpinBoxTop = QDoubleSpinBox(self.groupBoxTop)
+        self.doubleSpinBoxTop.setObjectName(u"doubleSpinBoxTop")
+        self.doubleSpinBoxTop.setGeometry(QRect(10, 20, 88, 24))
+
+        self.verticalLayout_7.addWidget(self.groupBoxTop)
+
+        self.groupBoxBottom = QGroupBox(self.groupBoxCursors)
+        self.groupBoxBottom.setObjectName(u"groupBoxBottom")
+        sizePolicy.setHeightForWidth(self.groupBoxBottom.sizePolicy().hasHeightForWidth())
+        self.groupBoxBottom.setSizePolicy(sizePolicy)
+        self.groupBoxBottom.setMinimumSize(QSize(120, 48))
+        self.doubleSpinBoxBotton = QDoubleSpinBox(self.groupBoxBottom)
+        self.doubleSpinBoxBotton.setObjectName(u"doubleSpinBoxBotton")
+        self.doubleSpinBoxBotton.setGeometry(QRect(10, 20, 88, 24))
+
+        self.verticalLayout_7.addWidget(self.groupBoxBottom)
+
+        self.groupBoxContrast = QGroupBox(self.groupBoxCursors)
+        self.groupBoxContrast.setObjectName(u"groupBoxContrast")
+        sizePolicy.setHeightForWidth(self.groupBoxContrast.sizePolicy().hasHeightForWidth())
+        self.groupBoxContrast.setSizePolicy(sizePolicy)
+        self.groupBoxContrast.setMinimumSize(QSize(120, 48))
+        self.plainTextEditContrast = QPlainTextEdit(self.groupBoxContrast)
+        self.plainTextEditContrast.setObjectName(u"plainTextEditContrast")
+        self.plainTextEditContrast.setGeometry(QRect(10, 20, 104, 21))
+        self.plainTextEditContrast.setReadOnly(True)
+
+        self.verticalLayout_7.addWidget(self.groupBoxContrast)
+
+
+        self.verticalLayout_4.addWidget(self.groupBoxCursors)
+
+        self.groupBox = QGroupBox(self.widget)
+        self.groupBox.setObjectName(u"groupBox")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy1)
         self.groupBox.setFlat(False)
         self.verticalLayout = QVBoxLayout(self.groupBox)
         self.verticalLayout.setObjectName(u"verticalLayout")
@@ -50,11 +111,11 @@ class Ui_Form(object):
 
         self.colormap_groub_box = QGroupBox(self.groupBox)
         self.colormap_groub_box.setObjectName(u"colormap_groub_box")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.colormap_groub_box.sizePolicy().hasHeightForWidth())
-        self.colormap_groub_box.setSizePolicy(sizePolicy1)
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.colormap_groub_box.sizePolicy().hasHeightForWidth())
+        self.colormap_groub_box.setSizePolicy(sizePolicy2)
         self.colormap_groub_box.setMinimumSize(QSize(120, 48))
         self.colormap_groub_box.setMaximumSize(QSize(120, 48))
         self.colormap_combo_box = QComboBox(self.colormap_groub_box)
@@ -73,52 +134,67 @@ class Ui_Form(object):
 
         self.verticalLayout.addWidget(self.auto_cross_check_box)
 
-        self.verticalSpacer = QSpacerItem(20, 210, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
         self.verticalLayout.addItem(self.verticalSpacer)
 
 
-        self.horizontalLayout_2.addWidget(self.groupBox)
+        self.verticalLayout_4.addWidget(self.groupBox)
 
-        self.verticalLayout_3 = QVBoxLayout()
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.output_beam_view = CustomGraphicsView(Form)
+        self.splitter_4.addWidget(self.widget)
+        self.splitter_3 = QSplitter(self.splitter_4)
+        self.splitter_3.setObjectName(u"splitter_3")
+        self.splitter_3.setOrientation(Qt.Orientation.Vertical)
+        self.verticalLayoutWidget_3 = QWidget(self.splitter_3)
+        self.verticalLayoutWidget_3.setObjectName(u"verticalLayoutWidget_3")
+        self.verticalLayout_6 = QVBoxLayout(self.verticalLayoutWidget_3)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.verticalLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.splitter_2 = QSplitter(self.verticalLayoutWidget_3)
+        self.splitter_2.setObjectName(u"splitter_2")
+        self.splitter_2.setOrientation(Qt.Orientation.Horizontal)
+        self.output_beam_view = CustomGraphicsView(self.splitter_2)
         self.output_beam_view.setObjectName(u"output_beam_view")
-
-        self.horizontalLayout.addWidget(self.output_beam_view)
-
-        self.verticalLayout_2 = QVBoxLayout()
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.cs_plot_x = PlotWidget(Form)
-        self.cs_plot_x.setObjectName(u"cs_plot_x")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.cs_plot_x.sizePolicy().hasHeightForWidth())
-        self.cs_plot_x.setSizePolicy(sizePolicy2)
-
-        self.verticalLayout_2.addWidget(self.cs_plot_x)
-
-        self.cs_plot_y = PlotWidget(Form)
+        self.splitter_2.addWidget(self.output_beam_view)
+        self.verticalLayoutWidget = QWidget(self.splitter_2)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayout_3 = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.splitter = QSplitter(self.verticalLayoutWidget)
+        self.splitter.setObjectName(u"splitter")
+        self.splitter.setOrientation(Qt.Orientation.Vertical)
+        self.cs_plot_y = PlotWidget(self.splitter)
         self.cs_plot_y.setObjectName(u"cs_plot_y")
-
-        self.verticalLayout_2.addWidget(self.cs_plot_y)
-
-
-        self.horizontalLayout.addLayout(self.verticalLayout_2)
-
-
-        self.verticalLayout_3.addLayout(self.horizontalLayout)
-
-        self.parametersGroupBox = QGroupBox(Form)
-        self.parametersGroupBox.setObjectName(u"parametersGroupBox")
-        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Expanding)
+        self.splitter.addWidget(self.cs_plot_y)
+        self.cs_plot_x = PlotWidget(self.splitter)
+        self.cs_plot_x.setObjectName(u"cs_plot_x")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         sizePolicy3.setHorizontalStretch(0)
         sizePolicy3.setVerticalStretch(0)
-        sizePolicy3.setHeightForWidth(self.parametersGroupBox.sizePolicy().hasHeightForWidth())
-        self.parametersGroupBox.setSizePolicy(sizePolicy3)
+        sizePolicy3.setHeightForWidth(self.cs_plot_x.sizePolicy().hasHeightForWidth())
+        self.cs_plot_x.setSizePolicy(sizePolicy3)
+        self.splitter.addWidget(self.cs_plot_x)
+
+        self.verticalLayout_3.addWidget(self.splitter)
+
+        self.splitter_2.addWidget(self.verticalLayoutWidget)
+
+        self.verticalLayout_6.addWidget(self.splitter_2)
+
+        self.splitter_3.addWidget(self.verticalLayoutWidget_3)
+        self.verticalLayoutWidget_2 = QWidget(self.splitter_3)
+        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
+        self.verticalLayout_5 = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.parametersGroupBox = QGroupBox(self.verticalLayoutWidget_2)
+        self.parametersGroupBox.setObjectName(u"parametersGroupBox")
+        sizePolicy4 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Expanding)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.parametersGroupBox.sizePolicy().hasHeightForWidth())
+        self.parametersGroupBox.setSizePolicy(sizePolicy4)
         self.gridLayout = QGridLayout(self.parametersGroupBox)
         self.gridLayout.setSpacing(1)
         self.gridLayout.setObjectName(u"gridLayout")
@@ -126,6 +202,11 @@ class Ui_Form(object):
         self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
         self.gridLayout.addItem(self.horizontalSpacer, 0, 1, 1, 1)
+
+        self.selectParametersPushButton = QPushButton(self.parametersGroupBox)
+        self.selectParametersPushButton.setObjectName(u"selectParametersPushButton")
+
+        self.gridLayout.addWidget(self.selectParametersPushButton, 0, 0, 1, 1)
 
         self.tableWidget = ParametersTableWidget(self.parametersGroupBox)
         self.tableWidget.setObjectName(u"tableWidget")
@@ -139,16 +220,13 @@ class Ui_Form(object):
 
         self.gridLayout.addWidget(self.tableWidget, 2, 0, 1, 2)
 
-        self.selectParametersPushButton = QPushButton(self.parametersGroupBox)
-        self.selectParametersPushButton.setObjectName(u"selectParametersPushButton")
 
-        self.gridLayout.addWidget(self.selectParametersPushButton, 0, 0, 1, 1)
+        self.verticalLayout_5.addWidget(self.parametersGroupBox)
 
+        self.splitter_3.addWidget(self.verticalLayoutWidget_2)
+        self.splitter_4.addWidget(self.splitter_3)
 
-        self.verticalLayout_3.addWidget(self.parametersGroupBox)
-
-
-        self.horizontalLayout_2.addLayout(self.verticalLayout_3)
+        self.horizontalLayout.addWidget(self.splitter_4)
 
 
         self.retranslateUi(Form)
@@ -158,6 +236,11 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.groupBoxCursors.setTitle(QCoreApplication.translate("Form", u"Cursors", None))
+        self.groupBoxMode.setTitle(QCoreApplication.translate("Form", u"Mode", None))
+        self.groupBoxTop.setTitle(QCoreApplication.translate("Form", u"Top", None))
+        self.groupBoxBottom.setTitle(QCoreApplication.translate("Form", u"Bottom", None))
+        self.groupBoxContrast.setTitle(QCoreApplication.translate("Form", u"Contrast", None))
         self.groupBox.setTitle(QCoreApplication.translate("Form", u"Controls", None))
         self.enable_check_box.setText(QCoreApplication.translate("Form", u"Enable", None))
         self.colormap_groub_box.setTitle(QCoreApplication.translate("Form", u"Colormap", None))
